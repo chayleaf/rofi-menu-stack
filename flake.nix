2fee2c47e553d2d6afa67bd4ef02ba107e623d03
chayleaf <chayleaf-git@pavluk.org>
2023-10-19 19:36:42 +0700

    flake: init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    packages = nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in rec {
+      rofi-menu-stack = pkgs.rustPlatform.buildRustPackage {
+        pname = "rofi-menu-stack";
+        version = "0.1.0";
+        src = pkgs.nix-gitignore.gitignoreSource ["*.nix"] (nixpkgs.lib.cleanSource ./.);
+        cargoLock.lockFile = ./Cargo.lock;
+        meta.license = nixpkgs.lib.licenses.mit;
+      };
+      default = rofi-menu-stack;
+    });
+  };
+}
