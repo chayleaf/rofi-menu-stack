22c7239d5da90174e53c58a8ddc24528edfc200d
chayleaf <chayleaf-git@pavluk.org>
2023-10-27 08:27:42 +0700

    samples: fix after lib update

--- sample/debug.sh
+++ sample/debug.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 . ./lib.sh
 options "{prompt:'Debug >',message:$(val "Current stack: $*"),fallback:{push:null}}"
-row "{text:'!!!!Pop',pop:1}"
-row "{text:'!!!!Pop 2',pop:2}"
-row "{text:'!!!!Jump to self',jump:'sample/debug.sh'}"
-row "{text:'!!!!Return',return:1}"
+raw "{text:'!!!!Pop',pop:1}"
+raw "{text:'!!!!Pop 2',pop:2}"
+raw "{text:'!!!!Jump to self',jump:'sample/debug.sh'}"
+raw "{text:'!!!!Return',return:1}"
