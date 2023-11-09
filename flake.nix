2b9790eea14f52b94d7846d25de5eec64493f55d
chayleaf <chayleaf-git@pavluk.org>
2023-11-09 19:22:05 +0700

    lib.nix: make it actually work (not fully tested)

--- flake.nix
+++ flake.nix
@@ -1,19 +1,61 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    packages = nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in rec {
-      rofi-menu-stack = pkgs.rustPlatform.buildRustPackage {
-        pname = "rofi-menu-stack";
-        version = "0.1.0";
-        src = pkgs.nix-gitignore.gitignoreSource ["*.nix"] (nixpkgs.lib.cleanSource ./.);
-        cargoLock.lockFile = ./Cargo.lock;
-        meta.license = nixpkgs.lib.licenses.mit;
-      };
-      default = rofi-menu-stack;
-    });
-    lib.x86_64-linux = import ./lib.nix { inherit (nixpkgs) lib; };
-  };
+  outputs = { self, nixpkgs }: 
+    let
+      forEachSystem = fn: nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ]
+        (system: fn (import nixpkgs {
+          inherit system;
+        }));
+    in
+    {
+      packages = forEachSystem (pkgs: rec {
+        rofi-menu-stack = pkgs.rustPlatform.buildRustPackage {
+          pname = "rofi-menu-stack";
+          version = "0.1.0";
+          src = pkgs.nix-gitignore.gitignoreSource ["*.nix"] (nixpkgs.lib.cleanSource ./.);
+          cargoLock.lockFile = ./Cargo.lock;
+          meta.license = nixpkgs.lib.licenses.mit;
+        };
+        test = let
+          inherit (nixpkgs) lib;
+          inherit (lib.modules) evalModules;
+          getVal = type: expr:
+            (evalModules {
+              modules = [
+                {
+                  options.val = lib.mkOption { inherit type; };
+                  config.val = expr;
+                }
+              ];
+            }).config.val;
+        in pkgs.writeShellScript "test-script" (self.lib.${pkgs.system}.compileMenu (getVal self.lib.${pkgs.system}.types.submenu {
+          prompt = "Fibonacci >";
+          message._bashStr = ''echo -n "Value #$1: $3"'';
+          rows = {
+            a = {
+              text = "Next";
+              pop = 3;
+              push = [
+                { _bashStr = ''echo -n "$2"''; }
+                { _bashStr = ''python3 -c "print($2+$3)"''; }
+                { _bashStr = ''echo -n "$(("$1" + 1))"''; }
+              ];
+            };
+            b = {
+              text = "Switch to incrementor";
+              pop = 2;
+              return = 1;
+              jump = "sample/incrementor.sh";
+            };
+            c = {
+              text = "Close";
+              return = null;
+            };
+          };
+        }));
+        default = rofi-menu-stack;
+      });
+      lib = forEachSystem (_: import ./lib.nix { inherit (nixpkgs) lib; });
+    };
 }
--- lib.nix
+++ lib.nix
@@ -2,18 +2,24 @@
 
 let self = {
   types = rec {
-    stringListNull = lib.types.oneOf [ bashStr lib.types.null (lib.types.listOf stringListNull) bashValue ];
+    stringListNull = lib.types.oneOf [ (lib.types.nullOr bashStr) (lib.types.listOf stringListNull) bashValue ];
     bashValue = lib.types.submodule {
       options._bash = lib.mkOption {
         description = "Bash code to execute that prints the desired JSON output";
-        type = lib.types.str;
+        type = with lib.types; nullOr str;
+        default = null;
+      };
+      options._bashStr = lib.mkOption {
+        description = "Bash code to execute that prints the desired string to get escaped as JSON";
+        type = with lib.types; nullOr str;
+        default = null;
       };
     };
     bashStr = lib.types.either lib.types.str bashValue;
     bashBool = lib.types.either lib.types.bool bashValue;
     bashInt = lib.types.either lib.types.int bashValue;
-    row' = cosmetic: lib.types.submodule {
-      options = {
+    row' = full: lib.types.submodule {
+      options = lib.optionalAttrs full {
         enable = lib.mkOption {
           default = true;
           type = lib.types.bool;
@@ -23,6 +29,7 @@ let self = {
           type = lib.types.nullOr lib.types.lines;
           default = null;
         };
+      } // {
         push = lib.mkOption {
           default = [ ];
           type = stringListNull;
@@ -48,10 +55,10 @@ let self = {
           type = bashBool;
         };
         menu = lib.mkOption {
-          default = {};
-          type = submenu;
+          default = null;
+          type = lib.types.nullOr (submenu' false);
         };
-      } // lib.optionalAttrs cosmetic {
+      } // lib.optionalAttrs full {
         text = lib.mkOption {
           type = bashStr;
         };
@@ -79,8 +86,8 @@ let self = {
     };
     row = row' true;
     fallbackRow = row' false;
-    submenu = lib.types.submodule {
-      options = {
+    submenu' = full: lib.types.submodule {
+      options = lib.optionalAttrs full {
         enable = lib.mkOption {
           default = true;
           type = lib.types.bool;
@@ -94,6 +101,11 @@ let self = {
           type = lib.types.nullOr lib.types.lines;
           default = null;
         };
+        rows = lib.mkOption {
+          default = [ ];
+          type = lib.types.attrsOf row;
+        };
+      } // {
         prompt = lib.mkOption {
           default = null;
           type = lib.types.nullOr bashStr;
@@ -114,31 +126,33 @@ let self = {
           default = null;
           type = lib.types.nullOr fallbackRow;
         };
-        rows = lib.mkOption {
-          default = [ ];
-          type = lib.types.attrsOf row;
-        };
       };
     };
+    submenu = submenu' true;
   };
 
-  escapeBashVal = x: if x?_bash then ''"$(${x._bash})"'' else lib.escapeShellArg (builtins.toJSON x);
-  filterJson = type: val: lib.filterAttrs (k: v: v != (type.getSubOptions { }).${k}.default) val;
+  escapeBashVal = x:
+    if (x._bash or null) != null then ''"$(${x._bash})"''
+    else if (x._bashStr or null) != null then ''"$(val "$(${x._bashStr})")"''
+    else if builtins.isList x then "[" + builtins.concatStringsSep "," (map self.escapeBashVal x) + "]"
+    else lib.escapeShellArg (builtins.toJSON x);
+  filterJson = type: val: lib.filterAttrs (k: v: (v.enable or true) && v != (type.getSubOptions { }).${k}.default) val;
 
-  compileRow = row: if row._bash != "" then row._bash else
+  compileRow = row: if row._bash != null then row._bash else
     builtins.concatStringsSep " " ([
       "row ${self.escapeBashVal row.text}"
     ] ++ lib.mapAttrsToList
       (k: v: "${k} ${self.escapeBashVal v}")
-      (self.filterJson self.types.row (builtins.removeAttrs row [ "enable" "_bash" ])));
-  compileOptions = options: if options._bash != "" then options._bash else
+      (self.filterJson self.types.row (builtins.removeAttrs row [ "enable" "_bash" "text" ])));
+  compileOptions = options: if options._bash != null then options._bash else
     builtins.concatStringsSep " " ([
       "options"
     ] ++ lib.mapAttrsToList
       (k: v: "${k} ${self.escapeBashVal v}")
       (self.filterJson self.types.submenu (builtins.removeAttrs options [ "enable" "execPre" "_bash" "rows" ])));
   compileMenu = menu: ''
+    . ${./lib.sh}
     ${self.compileOptions menu}
-    ${builtins.concatStringsSep "\n" (map self.compileRow menu.rows)}
+    ${builtins.concatStringsSep "\n" (map self.compileRow (builtins.attrValues menu.rows))}
   '';
 }; in self
